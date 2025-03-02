d539fb5582c91cb937c51b7d4a26a22f83028556
Your Name <you@example.com>
2025-03-03 00:32:42 +0300

    cmt

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -107,11 +107,11 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "Portal")
+#if WEBUI_NAME != "Portal":
+#    WEBUI_NAME += " (Portal)"
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "https://github.com/sorokinvld/portal-brandpack/blob/main/favicon.ico" #"https://openwebui.com/favicon.png"
 
 TRUSTED_SIGNATURE_KEY = os.environ.get("TRUSTED_SIGNATURE_KEY", "")
 
